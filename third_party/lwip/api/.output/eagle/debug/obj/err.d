e564125d72ae063cccd928f5514f64e616f2a9a1
zulolo <dodoleon@sina.com>
2017-04-16 16:34:14 +0800

    lwip library (.a and header) was changed.

---
+++ third_party/lwip/api/.output/eagle/debug/obj/err.d
@@ -0,0 +1,28 @@
+.output/eagle/debug/obj/err.o .output/eagle/debug/obj/err.d : err.c /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/err.h \
+ /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/opt.h \
+ /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwipopts.h \
+ /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/debug.h \
+ /home/zulolo/ESP8266_RTOS_SDK/include/lwip/lwip/arch.h \
+ /home/zulolo/ESP8266_RTOS_SDK/include/lwip/arch/cc.h \
+ /home/zulolo/ESP8266_RTOS_SDK/include/espressif/c_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdint.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/stdint.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/zulolo/ESP8266_RTOS_SDK/extra_include/xtensa/config/core-isa.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdbool.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/stdio.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stdarg.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/types.h \
+ /home/zulolo/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/stdio.h
